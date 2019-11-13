5931d91407e6bd2b5f3d4fd6ed100a69502dec7c
Harold Espinoza <haroldesptru@gmail.com>
2019-11-12 23:42:33 -0600

    Añadido componente de barra iconos de redes sociales

---
+++ src/components/SocialIcons.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './SocialIcons.css'
+
+function SocialIcons(){
+  return (
+    <div className='socialbar'>
+      <a className='fa fa-facebook' href="" title='Facebook'></a>
+      <a className='fa fa-twitter' href="" title='Twitter'></a>
+      <a className='fa fa-instagram' href="" title='Instagram'></a>
+    </div>
+  );
+}
+
+export default SocialIcons;
